648deeeb8c1ad8783795bedec438c9d5ba80f401
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-04 20:55:38 -0300

    first commit

---
+++ src/Model/ConnectDB.java
@@ -0,0 +1,66 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDB {
+    public static String status = "Não conectou...";
+
+    public ConnectDB() {
+
+    }
+
+    public static java.sql.Connection getConnection() {
+        Connection connection = null;
+
+        try {
+            String driverName = "com.mysql.jdbc.Driver";
+            Class.forName(driverName);
+
+            String serverName = "localhost";
+            String mydatabase = "mysql";
+            String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+            String username = "root";
+            String password = "123456";
+
+            connection = DriverManager.getConnection(url, username, password);
+
+            status = connection != null ? "STATUS---> Conexão realizada."
+                                        : "STATUS---> Não foi possivel realizar conexão";
+
+            return connection;
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println("O driver expecificado nao foi encontrado.");
+
+            return null;
+        }
+        catch (SQLException e) {
+            System.out.println("Nao foi possivel conectar ao Banco de Dados.");
+
+            return null;
+        }
+    }
+
+    public static String statusConnection() {
+        return status;
+    }
+
+    public static boolean closeConnection() {
+        try {
+            ConnectDB.getConnection().close();
+
+            return true;
+        }
+        catch (SQLException e) {
+
+            return false;
+        }
+    }
+
+    public static java.sql.Connection restartConnection() {
+        closeConnection();
+        return ConnectDB.getConnection();
+    }
+}
